9573c023b21c234478d8863acd1c3ce9da1c5b27
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2024-01-30 14:19:37 +0300

    Raycast

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    [SerializeField] private Vector3 rayDirection = new(0,0,10);
+    
+    void FixedUpdate()
+    {
+        var position = transform.position;
+
+        Debug.DrawRay(position, rayDirection * rayDirection.z, Color.red);
+        if (Physics.Raycast(position, rayDirection * rayDirection.z, out var hit))
+        {
+            //Print the name of the Collider your Box hit
+            Debug.Log($"Raycast hit:{hit.collider.name} from {gameObject.name}");
+        }
+    }
+}
